b41d53fd16a2045868ada3307c8bbabf23600d2e
unknown <opasnatikva@gmail.com>
2020-06-24 18:13:33 +0300

    added high score, renamed functions for clarity.

--- 2048.py
+++ 2048.py
@@ -20,34 +20,9 @@ def print_grid(grid):
         print(grid[index])
 
 
-# Check if there are any empty spaces on the board
-def board_full(list):
-    for index in list:
-        if index.count(" ") != 0:
-            return False
-    return True
-
-
-# Check if there are any possible moves
-def possible_moves(list):
-    for element in list:
-        for index in range(len(element)):
-            if element[index] == element[index + 1]:
-                return True
-    return False
-
 # Return a row of the board
 def get_row(grid, index):
     return grid[index]
-
-
-# Return a reversed order Line
-def reverse(line):
-    return line.reverse()
-
-
-#
-
 
 
 # Return a column of the board
@@ -58,14 +33,35 @@ def get_column(grid, index):
     return column
 
 
+# Return a reversed order Line
+def reverse(line):
+    return line.reverse()
+
+
+# Check if there are any empty spaces on the board
+def board_full(list):
+    for index in list:
+        if index.count(" ") != 0:
+            return False
+    return True
+
+
+# Check if there are any possible moves
+def moves_possible(grid):
+    for element in grid:
+        for index in range(len(element[1:-1])):
+            if element[index] == element[index + 1]:
+                return True
+    return False
+
 # Determine whether the new value to be inserted into an empty space is 2 or 4
-def input_value():
+def return_two_or_four():
     a = random.choices([2, 4], weights = [9, 1])
     return str(a[0])
 
 
 # Return empty cells from a line from the grid and sort them in a list
-def empty_cells(list):
+def return_empty_cells(list):
     empty_fields = []
     for index in range(len(list)):
         empty_fields.append(list[index])
@@ -92,10 +88,10 @@ def get_empty_cells(line):
 
 
 # Move all empty spaces defined by "get_empty_cells" to the end of the line
-def glue_values(line, empty):
-    for index in range(len(empty)):
-        line.append(line.pop(empty[index]))
-
+def shift_values(line, indexes):
+    for index in range(len(indexes)):
+         line.append(line.pop(indexes[index]))
+    return line
 
 # Return a list of the indexes of all the empty elements in the grid
 def all_the_empty_squares(list):
@@ -128,35 +124,38 @@ def take_input():
     return a
 
 
+def move_left(grid):
+    for index in range(len(grid)):
+        grid[index] = shift_values(get_row(grid, index), get_empty_cells(get_row(grid, index)))
+
+
+# High score counter (the sum of all numbers)
+def high_score(grid):
+    score = 0
+    for line in grid:
+        for element in line:
+            if element != " ":
+                score += int(element)
+    return score
+
+
 # Initiate Board
 board = generate_board(BOARD_SIZE)
+print(high_score(board))
 
 
 # Initial random number placements
-insert_random(random_empty_square(all_the_empty_squares(board)), input_value())
-
-
-insert_random(random_empty_square(all_the_empty_squares(board)), input_value())
-
-
-print_grid(board)
+insert_random(random_empty_square(all_the_empty_squares(board)), return_two_or_four())
+insert_random(random_empty_square(all_the_empty_squares(board)), return_two_or_four())
 
 
 # Begin turn sequence
-while not board_full(board) and possible_moves(board):
-    take_input()
+while moves_possible(board) and not board_full(board):
 
+    print_grid(board)
+    move_left(board)
+    insert_random(random_empty_square(all_the_empty_squares(board)), return_two_or_four())
+    print(high_score(board))
 
-    glue_values(board, get_empty_cells(all_the_empty_squares(board)))
-    insert_random(random_empty_square(all_the_empty_squares(board)), input_value())
-
+print_grid(board)
 print("Game Over!")
-
-
-def all to the left
-    take input
-    validate
-    extract lines in order
-    manipulate lines
-    write lines back to grid
-    insert new values in grid
